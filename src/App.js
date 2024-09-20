d17e85d4b8a1434c238bb7a33d5a3b0414e252cf
dafnapundak <dafna.m88@gmail.com>
2024-09-20 14:14:52 +0700

    refactoring hook use

--- src/App.js
+++ src/App.js
@@ -1,91 +1,86 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import axios from "axios";
 import Menu from "./components/Menu";
 import LandingPage from "./components/LandingPage";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: [],
+const App = () => {
+  // State using hooks
+  const [data, setData] = useState([]);
+
+  // useEffect to replace componentDidMount
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`/transactions`);
+      setData(response.data);
     };
-  }
 
-  async componentDidMount() {
-    const response = await axios.get(`/transactions`);
-    this.setState({ data: response.data });
-  }
+    fetchData();
+  }, []); // Empty dependency array mimics componentDidMount
 
-  async pushPosTransaction(newTransaction) {
+  // Function to handle positive transaction
+  const pushPosTransaction = async (newTransaction) => {
     newTransaction.amount = Number(newTransaction.amount);
     const response = await axios.post("/transaction", newTransaction);
-    let data = [...this.state.data];
-    data.push(response.data);
-    this.setState({ data });
-  }
+    setData([...data, response.data]);
+  };
 
-  async pushNegTransaction(newTransaction) {
+  // Function to handle negative transaction
+  const pushNegTransaction = async (newTransaction) => {
     newTransaction.amount = Number("-" + newTransaction.amount);
     const response = await axios.post("/transaction", newTransaction);
-    let data = [...this.state.data];
-    data.push(response.data);
-    this.setState({ data });
-  }
+    setData([...data, response.data]);
+  };
 
-  async deleteTransaction(transactionToDelete) {
+  // Function to handle deletion of a transaction
+  const deleteTransaction = async (transactionToDelete) => {
     await axios.delete("/transaction", {
       data: transactionToDelete,
     });
-    let data = [...this.state.data];
-    for (let i = 0; i < data.length; i++) {
-      if (data[i]._id === transactionToDelete.id) {
-        data.splice(i, 1);
-        this.setState({ data });
-      }
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <Router>
-          <Route
-            path="/"
-            exact
-            render={() => <LandingPage items={this.state.data} />}
-          />
-          <Route
-            path="/menu"
-            exact
-            render={() => (
-              <Menu
-                index={0}
-                items={this.state.data}
-                pushPosTransaction={this.pushPosTransaction}
-                pushNegTransaction={this.pushNegTransaction}
-                deleteTransaction={this.deleteTransaction}
-              />
-            )}
-          />
-          <Route
-            path="/transactions"
-            exact
-            render={() => (
-              <Menu
-                index={1}
-                items={this.state.data}
-                pushPosTransaction={this.pushPosTransaction}
-                pushNegTransaction={this.pushNegTransaction}
-                deleteTransaction={this.deleteTransaction}
-              />
-            )}
-          />
-        </Router>
-      </div>
+    const updatedData = data.filter(
+      (transaction) => transaction._id !== transactionToDelete.id
     );
-  }
-}
+    setData(updatedData);
+  };
+
+  return (
+    <div>
+      <Router>
+        <Route
+          path="/"
+          exact
+          render={() => <LandingPage items={data} />}
+        />
+        <Route
+          path="/menu"
+          exact
+          render={() => (
+            <Menu
+              index={0}
+              items={data}
+              pushPosTransaction={pushPosTransaction}
+              pushNegTransaction={pushNegTransaction}
+              deleteTransaction={deleteTransaction}
+            />
+          )}
+        />
+        <Route
+          path="/transactions"
+          exact
+          render={() => (
+            <Menu
+              index={1}
+              items={data}
+              pushPosTransaction={pushPosTransaction}
+              pushNegTransaction={pushNegTransaction}
+              deleteTransaction={deleteTransaction}
+            />
+          )}
+        />
+      </Router>
+    </div>
+  );
+};
 
 export default App;
