5db63010d3abdb1f8bf61489f5060329abe04060
dafnapundak <dafna.m88@gmail.com>
2024-09-24 11:57:48 +0700

    update cors option

--- api/server.js
+++ api/server.js
@@ -9,7 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: ['https://kidswallet.vercel.app/', 'http://localhost:4000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Connect to MongoDB
